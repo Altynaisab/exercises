c09b9ed013759abe2646963a3153fbec5af3783a
Altynai <altynai.sabetova@gmail.com>
2019-03-28 09:29:22 +0600

    exercises till 19

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("Whats your name?", end=' ')
+name = input()
+print("Where are you live?", end=' ')
+adress = input()
+print("Where is your hometown?", end=' ')
+city = input()
+
+print(f"So, you're {name}, you live in {adress} and your hometown is {city}.")
---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"#there are listed days
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"#here listed months
+
+print("Here are the days: ", days)#gonna print days
+print("Here are the months: ", months)#gonna print months
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6 .
+""")#testing triple quotes
