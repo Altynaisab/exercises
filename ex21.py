33a7fbebd682f6216a8328f9eedf9293bb287a09
Altynai <altynai.sabetova@gmail.com>
2019-03-29 04:08:55 +0600

    exercises 20-30 {23}

---
+++ ex21.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+Adding = add(5,2)
+Subtracting = subtract(7, 2)
+Multiplying = multiply(3, 4)
+Dividing = divide(3, 6)
+
+print("function Adding is ", Adding)
+print("function Subtracting is ", Subtracting)
+print("function Multiplying is ", Multiplying)
+print("function Dividing is ", Dividing)
---
+++ ex29.py
@@ -0,0 +1,20 @@
+smartphones = 20
+notebooks = 30
+books = 10
+
+if smartphones < notebooks:
+    print("Too many notebooks!!")
+
+if smartphones > notebooks:
+    print("Too many smartphones!!")
+
+if smartphones < books:
+    print("Good, people like to read hardcopy!")
+
+if smartphones > books:
+    print("So, now books are too expensive to buy them, I cannot to venture..(")
+
+books += 10
+
+if smartphones >= books:
+    print("Maybe for someones comfortable to read hardcopy or not")
